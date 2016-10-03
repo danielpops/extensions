4d8c4cef7d36cd1c84b8d938b6ec6bfe2c00c53b
Daniel Popescu <danielpops@gmail.com>
2016-10-02 20:20:58 -0700

    Add the main code in copy.js to do the node traversal and actually do the copy to clipboard functionality

--- link-url-to-clipboard/copy.js
+++ link-url-to-clipboard/copy.js
@@ -11,4 +11,35 @@ function copyTextToClipboard(text) {
       document.execCommand('copy');
       body.removeChild(copyFrom);
 }
+document.addEventListener('keydown', doKeyPress); //add the keyboard handler
+
+function doKeyPress(e){
+    if (e.metaKey == true && e.keyCode == 67){ // if e.shiftKey is not provided then script will run at all instances of typing "G"
+        console.log("Hijacked cmd + c event");
+        // Find the nearest a href link tag in the node hierarchy
+        selection = document.getSelection();
+        if(selection.toString() == "") {
+            return false;
+        }
+        node = selection.focusNode;
+        console.log("Initial node name: " + node.nodeName);
+        while(node.parentElement != null) {
+            node = node.parentElement;
+            console.log("Node name: " + node.nodeName);
+            if(node.nodeName == "A") {
+                break;
+            }
+        }
+        url = node.href;
+        if(url == null) {
+            console.log("Couldn't find a link. Just do whatever it would have done anyway");
+            return false;
+        }
+        console.log("Copying the url to clipboard: " + url);
+        copyTextToClipboard(url);
+        return true;
+    }
+
+    return false;
+}
 
