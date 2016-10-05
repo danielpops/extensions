525f5ffd4bb882bf315d97eb726582e33fdcbbc9
Jake Bakhoum <jake@chartboost.com>
2016-10-05 00:10:58 -0700

    localStorage suss

--- link-url-to-clipboard/background.js
+++ link-url-to-clipboard/background.js
@@ -2,10 +2,19 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log("got to background.");
         // read `newIconPath` from request and read `tab.id` from sender
-        if(request.hasOwnProperty("changeIcon")==true){
-          console.log('message passing is working');
-          chrome.browserAction.setIcon({
-              path: "writeit.png",
-          });         
-        }
+        if(request.hasOwnProperty("changeIcon")){
+        	localStorage.setItem("shoudCopyURLS", !localStorage.getItem("shoudCopyURLS"));
+        	console.log("turning on extension");
+        	if(localStorage.getItem("shoudCopyURLS")){
+	            chrome.browserAction.setIcon({
+	              path: "writeit.png"
+	          	});   		
+        	}
+        	if(!localStorage.getItem("shoudCopyURLS")){
+	            chrome.browserAction.setIcon({
+	              path: "plain.png"
+	          	});   		
+        	}
+        
+        }       
     });
--- link-url-to-clipboard/copy.js
+++ link-url-to-clipboard/copy.js
@@ -2,6 +2,7 @@
  * Create a temporary hidden element, put some text on there, and copy to the system clipboard
  *
 */
+
 function copyTextToClipboard(text) {
       var copyFrom = document.createElement("textarea");
       copyFrom.textContent = text;
@@ -11,42 +12,52 @@ function copyTextToClipboard(text) {
       document.execCommand('copy');
       body.removeChild(copyFrom);
 }
+
 document.addEventListener('keydown', doKeyPress); //add the keyboard handler
 
 function doKeyPress(e){
 
+
+	if(localStorage.getItem("shoudCopyURLS") == null){
+		localStorage.setItem("shoudCopyURLS", false);
+		console.log("initializing localStorage");
+	}
+
 	if (e.metaKey == true && e.keyCode == 190){ 
-		chrome.runtime.sendMessage({changeIcon:true}, function(response) {
-		  console.log("sending message to background.");
+		chrome.runtime.sendMessage({changeIcon:!localStorage["shoudCopyURLS"]}, function(response) {
+		  console.log("sending message to background and toggling extension.");
 		});
 	}
 
-    if (e.metaKey == true && e.keyCode == 67){ // if e.shiftKey is not provided then script will run at all instances of typing "G"
-        console.log("Hijacked cmd + c event");
-        // Find the nearest a href link tag in the node hierarchy
-        selection = document.getSelection();
-        if(selection.toString() == "") {
-            return false;
-        }
-        node = selection.focusNode;
-        console.log("Initial node name: " + node.nodeName);
-        while(node.parentElement != null) {
-            node = node.parentElement;
-            console.log("Node name: " + node.nodeName);
-            if(node.nodeName == "A") {
-                break;
-            }
-        }
-        url = node.href;
-        if(url == null) {
-            console.log("Couldn't find a link. Just do whatever it would have done anyway");
-            return false;
-        }
-        console.log("Copying the url to clipboard: " + url);
-        copyTextToClipboard(url);
-        return true;
-    }
+	if(localStorage["shoudCopyURLS"] == true){
+	    if (e.metaKey == true && e.keyCode == 67){ // if e.shiftKey is not provided then script will run at all instances of typing "G"
+	        console.log("Hijacked cmd + c event");
+	        // Find the nearest a href link tag in the node hierarchy
+	        selection = document.getSelection();
+	        if(selection.toString() == "") {
+	            return false;
+	        }
+	        node = selection.focusNode;
+	        console.log("Initial node name: " + node.nodeName);
+	        while(node.parentElement != null) {
+	            node = node.parentElement;
+	            console.log("Node name: " + node.nodeName);
+	            if(node.nodeName == "A") {
+	                break;
+	            }
+	        }
+	        url = node.href;
+	        if(url == null) {
+	            console.log("Couldn't find a link. Just do whatever it would have done anyway");
+	            return false;
+	        }
+	        console.log("Copying the url to clipboard: " + url);
+	        copyTextToClipboard(url);
+	        return true;
+	    }
 
-    return false;
+	    return false;		
+	}
+
 }
 
